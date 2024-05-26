9fe9ad1206427ebd8b364a7c9b6588fcd85d84a2
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-27 00:31:33 +0300

    add route admin/home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BackEnd\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/home', [HomeController::class, 'index']);
