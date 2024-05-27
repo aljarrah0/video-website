c9217937f5fe49e64d5cefdc852f59f205e7d11c
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-27 12:25:22 +0300

    update blade

--- resources/views/back-end/home.blade.php
+++ resources/views/back-end/home.blade.php
@@ -1,6 +1,20 @@
-@extends('back-end.layout.app')
+@extends('back-end.layout.app', ['title' => 'Home Page'])
 
+@push('css')
+    <style>
+        a {
+            color: red !important ;
+        }
+    </style>
+@endpush
 
 @section('content')
+
     <h1>Home Page</h1>
 @endsection
+
+@push('js')
+    <script>
+        alert('Hi World')
+    </script>
+@endpush
