c6d2d1e4c38b8d4f62e95a8faabe164c9b818682
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 02:47:55 +0300

    add Validation and show error massages

--- resources/views/back-end/users/form.blade.php
+++ resources/views/back-end/users/form.blade.php
@@ -2,19 +2,34 @@
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">الاسم</label>
-            <input type="text" name="name" value="{{ old('name', isset($row) ? $row->name : '') }}" class="form-control">
+            <input type="text" name="name" value="{{ old('name', isset($row) ? $row->name : '') }}" class="form-control @error('name') is-invalid @enderror">
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">الإيميل</label>
-            <input type="email" name="email" value="{{ old('email', isset($row) ? $row->email : '') }}" class="form-control">
+            <input type="email" name="email" value="{{ old('email', isset($row) ? $row->email : '') }}" class="form-control @error('email') is-invalid @enderror">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">كلمة المرور</label>
-            <input type="password" name="password" class="form-control">
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
 </div>
