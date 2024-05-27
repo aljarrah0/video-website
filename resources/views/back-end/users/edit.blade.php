57d1766beb786503d3aeded3fa0ccd261ee15a2f
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 01:07:54 +0300

    add edit page

---
+++ resources/views/back-end/users/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('back-end.layout.app', ['title' => $title])
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header card-header-primary">
+                    <h4 class="card-title">{{ $title }}</h4>
+                    <p class="card-category"></p>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('admin.users.update', $user->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        @include('back-end.users.form')
+                        <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
+                        <div class="clearfix"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
