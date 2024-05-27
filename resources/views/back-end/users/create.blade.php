a73c025188da1b092c46ac020354f5cd6edef880
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 01:07:35 +0300

    update create page

--- resources/views/back-end/users/create.blade.php
+++ resources/views/back-end/users/create.blade.php
@@ -13,26 +13,7 @@
                 <div class="card-body">
                     <form action="{{ route('admin.users.store') }}" method="POST">
                         @csrf
-                        <div class="row">
-                        <div class="col-md-4">
-                            <div class="form-group bmd-form-group">
-                            <label class="bmd-label-floating">الاسم</label>
-                            <input type="text" name="name" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group bmd-form-group">
-                            <label class="bmd-label-floating">الإيميل</label>
-                            <input type="email" name="email" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group bmd-form-group">
-                            <label class="bmd-label-floating">كلمة المرور</label>
-                            <input type="password" name="password" class="form-control">
-                            </div>
-                        </div>
-                        </div>
+                        @include('back-end.users.form')
                         <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
                         <div class="clearfix"></div>
                     </form>
