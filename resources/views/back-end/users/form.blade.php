c80635d26702b4acc3ca2017887b51b354ebbbe7
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 22:52:49 +0300

    enhancement form views

--- resources/views/back-end/users/form.blade.php
+++ resources/views/back-end/users/form.blade.php
@@ -1,31 +1,34 @@
 <div class="row">
+    @php $input = 'name' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">الاسم</label>
-            <input type="text" name="name" value="{{ old('name', isset($row) ? $row->name : '') }}" class="form-control @error('name') is-invalid @enderror">
-            @error('name')
+            <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
+            @error($input)
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
         </div>
     </div>
+    @php $input = 'email' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">الإيميل</label>
-            <input type="email" name="email" value="{{ old('email', isset($row) ? $row->email : '') }}" class="form-control @error('email') is-invalid @enderror">
-            @error('email')
+            <input type="email" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
+            @error($input)
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
         </div>
     </div>
+    @php $input = 'password' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">كلمة المرور</label>
-            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
-            @error('password')
+            <input type="password" name="{{ $input }}" class="form-control @error($input) is-invalid @enderror">
+            @error($input)
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
