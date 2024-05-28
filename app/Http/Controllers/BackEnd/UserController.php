9af6aaee173d6c650032ad1b88fb637237f520d9
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 03:07:04 +0300

    update UserController

--- app/Http/Controllers/BackEnd/UserController.php
+++ app/Http/Controllers/BackEnd/UserController.php
@@ -14,17 +14,13 @@ class UserController extends BackEndController
 
     public function store(UserRequest $request)
     {
-        User::create($request->validated());
+        $this->model->create($request->validated());
 
         return redirect()->route('admin.users.index')->with('تم إنشاء المستخدم بنجاح');
     }
 
     public function update(UserRequest $request, User $user)
     {
-        $request->validate([
-            'email' => ['required', 'email', 'unique:users,email,'.$user->id],
-        ]);
-
         $user->update([
             'name' => $request->name,
             'email' => $request->email,
