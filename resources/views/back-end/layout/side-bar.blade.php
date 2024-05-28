6eea7fe0986b7272e0d053a1c852ff8e628be31f
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-29 02:04:51 +0300

    update side-bar

--- resources/views/back-end/layout/side-bar.blade.php
+++ resources/views/back-end/layout/side-bar.blade.php
@@ -11,10 +11,30 @@
     </div>
     <div class="sidebar-wrapper">
         <ul class="nav">
-            <li class="nav-item active  ">
-                <a class="nav-link" href="javascript:void(0)">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.index') }}">
                     <i class="material-icons">dashboard</i>
                     <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.users.index') }}">
+                    <i class="material-icons">people</i>
+                    <p>{{ trans('app.users.index') }}</p>
+                </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.categories.index') }}">
+                    <i class="material-icons">category</i>
+                    <p>{{ trans('app.categories.index') }}</p>
+
+                </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.skills.index') }}">
+                    <i class="material-icons">content_paste</i>
+                    <p>{{ trans('app.skills.index') }}</p>
+
                 </a>
             </li>
             <!-- your sidebar here -->
