195db9fb7fac8df12318bad373d6471c5a01ad70
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 22:28:29 +0300

    enhancement  create page

--- resources/views/back-end/users/create.blade.php
+++ resources/views/back-end/users/create.blade.php
@@ -1,22 +1,12 @@
 @extends('back-end.layout.app', ['title' => $title])
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header card-header-primary">
-                    <h4 class="card-title">{{ $title }}</h4>
-                    <p class="card-category"></p>
-                </div>
-                <div class="card-body">
-                    <form action="{{ route('admin.users.store') }}" method="POST">
-                        @csrf
-                        @include('back-end.users.form')
-                        <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
-                        <div class="clearfix"></div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    @component('back-end.shared.create', ['title' => $title])
+        <form action="{{ route('admin.users.store') }}" method="POST">
+            @csrf
+            @include('back-end.users.form')
+            <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
+            <div class="clearfix"></div>
+        </form>
+    @endcomponent
 @endsection
