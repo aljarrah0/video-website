906957b41c3b9c280ee5a52f0ef75eb5abedcc7a
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 19:55:27 +0300

    enhancement index view

---
+++ resources/views/back-end/shared/index.blade.php
@@ -0,0 +1,20 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header card-header-primary">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h4 class="card-title ">{{ $title }}</h4>
+                        <p class="card-category"></p>
+                    </div>
+                    <div class="col-md-4 text-right">
+                        <a href="{{ route('admin.'.$model.'.create') }}" class="btn btn-white btn-round">{{ $create }}</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/back-end/users/index.blade.php
+++ resources/views/back-end/users/index.blade.php
@@ -1,63 +1,47 @@
 @extends('back-end.layout.app')
 
+
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-        <div class="card">
-            <div class="card-header card-header-primary">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h4 class="card-title ">{{ $title }}</h4>
-                        <p class="card-category"></p>
-                    </div>
-                    <div class="col-md-4 text-right">
-                        <a href="{{ route('admin.users.create') }}" class="btn btn-white btn-round">{{ $create }}</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead class=" text-primary">
+    @component('back-end.shared.index', ['title' => $title, 'model' => $model, 'create' => $create, 'rows' => $rows, 'edit' => $edit, 'delete' => $delete])
+        <div class="table-responsive">
+            <table class="table">
+                <thead class=" text-primary">
+                    <tr>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            E-mail
+                        </th>
+                        <th class="text-right">
+                            الإجراءات
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($rows as $row)
                         <tr>
-                            <th>
-                                ID
-                            </th>
-                            <th>
-                                Name
-                            </th>
-                            <th>
-                                E-mail
-                            </th>
-                            <th class="text-right">
-                                الإجراءات
-                            </th>
+                            <td>
+                                {{ $loop->iteration }}
+                            </td>
+                            <td>
+                                {{ $row->name}}
+                            </td>
+                            <td>
+                                {{ $row->email }}
+                            </td>
+                            <td class="td-actions text-right">
+                                @include('back-end.shared.buttons.edit')
+                                @include('back-end.shared.buttons.delete')
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($rows as $row)
-                            <tr>
-                                <td>
-                                    {{ $loop->iteration }}
-                                </td>
-                                <td>
-                                    {{ $row->name}}
-                                </td>
-                                <td>
-                                    {{ $row->email }}
-                                </td>
-                                <td class="td-actions text-right">
-                                    @include('back-end.shared.buttons.edit')
-                                    @include('back-end.shared.buttons.delete')
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-        </div>
-    </div>
+    @endcomponent
 @endsection
 
