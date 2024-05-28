e39cf6b23311e90d9c942e8e93db3e3388102f83
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 22:55:44 +0300

    enhancement form views

--- resources/views/back-end/users/form.blade.php
+++ resources/views/back-end/users/form.blade.php
@@ -2,7 +2,7 @@
     @php $input = 'name' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
-            <label class="bmd-label-floating">الاسم</label>
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
             <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
             @error($input)
                 <span class="invalid-feedback" role="alert">
@@ -14,7 +14,7 @@
     @php $input = 'email' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
-            <label class="bmd-label-floating">الإيميل</label>
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
             <input type="email" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
             @error($input)
                 <span class="invalid-feedback" role="alert">
@@ -26,7 +26,7 @@
     @php $input = 'password' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
-            <label class="bmd-label-floating">كلمة المرور</label>
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
             <input type="password" name="{{ $input }}" class="form-control @error($input) is-invalid @enderror">
             @error($input)
                 <span class="invalid-feedback" role="alert">
