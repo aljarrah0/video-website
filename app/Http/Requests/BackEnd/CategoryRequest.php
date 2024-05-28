a3e84463c928efd652704d3f5c34190b4d24e3a2
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-29 00:28:14 +0300

    add CategoryRequest

---
+++ app/Http/Requests/BackEnd/CategoryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\BackEnd;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $commonRules = [
+            'name' => ['required', 'string', 'min:3', 'max:255'],
+            'meta_keywords' => ['nullable', 'string', 'min:3', 'max:255'],
+            'meta_description' => ['nullable', 'string', 'min:3', 'max:255'],
+
+        ];
+
+        if ($this->method() == 'POST') {
+            $specialRules = [
+            ];
+        } elseif ($this->method() == 'PUT') {
+            $specialRules = [
+            ];
+        }
+
+        return $commonRules + $specialRules;
+    }
+}
