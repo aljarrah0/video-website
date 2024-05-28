bca19df03f197b9202e6eaeeaa23804e501b4cd2
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-28 22:32:09 +0300

    enhancement edit page

--- resources/views/back-end/users/edit.blade.php
+++ resources/views/back-end/users/edit.blade.php
@@ -1,23 +1,13 @@
 @extends('back-end.layout.app', ['title' => $title])
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header card-header-primary">
-                    <h4 class="card-title">{{ $title }}</h4>
-                    <p class="card-category"></p>
-                </div>
-                <div class="card-body">
-                    <form action="{{ route('admin.users.update', $row->id) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        @include('back-end.users.form')
-                        <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
-                        <div class="clearfix"></div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    @component('back-end.shared.edit', ['title' => $title])
+        <form action="{{ route('admin.users.update', $row) }}" method="POST">
+            @csrf
+            @method('PUT')
+            @include('back-end.users.form')
+            <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
+            <div class="clearfix"></div>
+        </form>
+    @endcomponent
 @endsection
