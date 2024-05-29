bfcbaca4d7d27467de518cf97c7888205f4b3ca3
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-30 00:35:08 +0300

    add views pages

---
+++ resources/views/back-end/pages/form.blade.php
@@ -0,0 +1,34 @@
+<div class="row">
+    @php $input = 'name' @endphp
+    <div class="col-md-6">
+        <div class="form-group bmd-form-group">
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
+            <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
+            @include('back-end.shared.error')
+        </div>
+    </div>
+    @php $input = 'meta_keywords' @endphp
+    <div class="col-md-6">
+        <div class="form-group bmd-form-group">
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
+            <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
+            @include('back-end.shared.error')
+        </div>
+    </div>
+    @php $input = 'meta_description' @endphp
+    <div class="col-md-6">
+        <div class="form-group bmd-form-group">
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
+            <textarea class="form-control @error($input) is-invalid @enderror" name="{{ $input }}" id="" cols="30" rows="10">{{ old($input, isset($row) ? $row->{$input} : '') }}</textarea>
+            @include('back-end.shared.error')
+        </div>
+    </div>
+    @php $input = 'description' @endphp
+    <div class="col-md-6">
+        <div class="form-group bmd-form-group">
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
+            <textarea class="form-control @error($input) is-invalid @enderror" name="{{ $input }}" id="" cols="30" rows="10">{{ old($input, isset($row) ? $row->{$input} : '') }}</textarea>
+            @include('back-end.shared.error')
+        </div>
+    </div>
+</div>
