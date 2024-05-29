7b9c174a810ff04a9868ecee639a6c8283219bb1
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-29 23:52:50 +0300

    add views tags

---
+++ resources/views/back-end/tags/form.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    @php $input = 'name' @endphp
+    <div class="col-md-6">
+        <div class="form-group bmd-form-group">
+            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
+            <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
+            @error($input)
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+</div>
