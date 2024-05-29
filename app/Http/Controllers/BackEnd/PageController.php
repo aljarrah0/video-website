00eabefc84d63dfa2db00a42678c266b76e337ba
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-30 00:27:27 +0300

    add Page Controller

---
+++ app/Http/Controllers/BackEnd/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\BackEnd\BackEndController;
+use App\Http\Requests\BackEnd\PageRequest;
+use App\Models\Page;
+
+class PageController extends BackEndController
+{
+    public function __construct(Page $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function store(PageRequest $request)
+    {
+        $this->model->create($request->validated());
+
+        return redirect()->route('admin.'.$this->getModelName().'.index');
+    }
+
+    public function update(PageRequest $request, Page $page)
+    {
+        $page->update($request->validated());
+
+        return redirect()->route('admin.'.$this->getModelName().'.index');
+    }
+}
