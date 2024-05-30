cca21e1465aedcc63742f6b7b13f135601542949
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-30 09:11:12 +0300

    add VideoController

---
+++ app/Http/Controllers/BackEnd/VideoController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\BackEnd;
+
+use App\Http\Requests\BackEnd\VideoRequest;
+use App\Models\Category;
+use App\Models\Video;
+
+class VideoController extends BackEndController
+{
+    public function __construct(Video $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function create()
+    {
+        $title = trans('app.'.$this->getModelName().'.create');
+        $model = $this->getModelName();
+        $categories = Category::get(['id', 'name']);
+
+        return view('back-end.'.$this->getModelName().'.create', get_defined_vars());
+    }
+
+    public function store(VideoRequest $request)
+    {
+        $this->model->create($request->validated() + ['user_id' => auth()->user()->id]);
+
+        return redirect()->route('admin.'.$this->getModelName().'.index');
+    }
+
+    public function edit($id)// هل استطيع أن احافظ على مفهوم injection
+    {
+        $title = trans('app.'.$this->getModelName().'.edit');
+        $model = $this->getModelName();
+        $row = $this->model->findOrFail($id);
+        $categories = Category::get(['id', 'name']);
+
+        return view('back-end.'.$this->getModelName().'.edit', get_defined_vars());
+    }
+
+    public function update(VideoRequest $request, Video $video)
+    {
+        $video->update($request->validated());
+
+        return redirect()->route('admin.'.$this->getModelName().'.index');
+    }
+}
