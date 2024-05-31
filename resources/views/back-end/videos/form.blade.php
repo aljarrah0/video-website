dfd92c1f2fa8df2bd0b88c9a2bc5463e0d3f4e11
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-01 00:40:38 +0300

    update form videso

--- resources/views/back-end/videos/form.blade.php
+++ resources/views/back-end/videos/form.blade.php
@@ -16,10 +16,17 @@
         </div>
     </div>
     @php $input = 'youtube' @endphp
-    <div class="col-md-12">
+    <div class="col-md-6">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
             <input type="url" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
+            @include('back-end.shared.error')
+        </div>
+    </div>
+    @php $input = 'image' @endphp
+    <div class="col-md-6">
+        <div class="">
+            <input type="file" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" accept="*.jpg" class="form-control @error($input) is-invalid @enderror m-2">
             @include('back-end.shared.error')
         </div>
     </div>
