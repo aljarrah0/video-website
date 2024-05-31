6f6ad6164ebef48c1dab78ba8be82e63be950c0c
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-31 10:54:16 +0300

    add relationship in tow tables: skills, videos

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Video extends Model
 {
@@ -20,5 +21,10 @@ class Video extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function skills(): BelongsToMany
+    {
+        return $this->belongsToMany(Skill::class, 'skill_video');
     }
 }
