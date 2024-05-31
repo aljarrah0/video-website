b20a5c4be06ee7345b3bb69edc9017220dceb36d
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-31 17:41:25 +0300

    update edit shared (add video view)

--- resources/views/back-end/shared/edit.blade.php
+++ resources/views/back-end/shared/edit.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
         <div class="card">
             <div class="card-header card-header-primary">
                 <h4 class="card-title">{{ $title }}</h4>
@@ -10,4 +10,14 @@
             </div>
         </div>
     </div>
+    @if (isset($video))
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    {{ isset($video)? $video : '' }}
+                    {{-- {{ $video }} --}}
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
