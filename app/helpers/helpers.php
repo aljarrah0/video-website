1548bee550c1bf56e53bd2b25c0faa8a994f5f1d
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-31 17:35:11 +0300

    add getYoutubeId

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -4,3 +4,10 @@ function is_active(string $routeName)
 {
     return request()->segment(2) != null && request()->segment(2) == $routeName ? 'active' : '';
 }
+
+function getYoutubeId($url)
+{
+    preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
+
+    return isset($match[1]) ? $match[1] : null;
+}
