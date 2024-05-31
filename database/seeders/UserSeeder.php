e247133f3e6bdb33ab94948f50912a75486d19d8
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-31 10:54:45 +0300

    update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,8 +14,9 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'mohammed aljarrah',
+            'email' => 'moh.moh.aljarrah@gmail.com',
+            'password' => Hash::make('moh.moh.aljarrah@gmail.com'),
         ]);
 
         User::factory()->count(10)->create();
