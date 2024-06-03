4e486b5bd872290152984355395c75911b4c42da
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-03 20:47:16 +0300

    update edit videos

--- resources/views/back-end/videos/edit.blade.php
+++ resources/views/back-end/videos/edit.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
     @component('back-end.shared.edit', ['title' => $title])
-        <form action="{{ route('admin.'.$model.'.update', $row) }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('admin.' . $model . '.update', $row) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
-            @include('back-end.'.$model.'.form')
+            @include('back-end.' . $model . '.form')
             <button type="submit" class="btn btn-primary pull-right">{{ $title }}</button>
             <div class="clearfix"></div>
         </form>
         @slot('video')
             @php
-                $url = getYoutubeId($row->youtube)
+                $url = getYoutubeId($row->youtube);
             @endphp
             @if ($url)
-                <iframe width="400" src="https://www.youtube.com/embed/{{ $url }}" style="margin-bottom: 20px" allowfullscreen></iframe>
+                <iframe width="400" src="https://www.youtube.com/embed/{{ $url }}" style="margin-bottom: 20px"
+                    allowfullscreen></iframe>
             @endif
-            <img src="{{ url('uploads/'.$row->image) }}" width="400">
+            <img src="{{ url('uploads/' . $row->image) }}" width="400">
         @endslot
     @endcomponent
 @endsection
