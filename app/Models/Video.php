c3fe1d3967e7df760953b73375af37485904429d
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-03 21:15:53 +0300

    add relationship tables

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Video extends Model
 {
@@ -31,5 +32,10 @@ class Video extends Model
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'tag_video');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
