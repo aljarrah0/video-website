c0f49a7c4a9bef7c819736844323448e184ede11
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-06 22:49:13 +0300

    update index videos

--- resources/views/back-end/comments/index.blade.php
+++ resources/views/back-end/comments/index.blade.php
@@ -1,10 +1,18 @@
-@forelse ($comments as $comment)
-    <tr>
-        <td>{{ $loop->iteration }}:</td>
-        <td>{{ $comment->comment }}</td><br>
-    </tr>
-@empty
-    <tr>
-        <td class="text-center" colspan="10">{{ trans('app.no_data') }}</td>
-    </tr>
-@endforelse
+<table class="table">
+    <tbody>
+        @forelse ($comments as $comment)
+            <tr>
+                <td>{{ $loop->iteration }}:</td>
+                <td>
+                    <small>{{ $comment->user->name }}</small>
+                    <p>{{ $comment->comment }}</p>
+                    <small>{{ $comment->created_at }}</small>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td class="text-center" colspan="10">{{ trans('app.no_data') }}</td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
