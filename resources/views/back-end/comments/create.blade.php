36a4f2b39a002ef26246045e5df6fe5fda3cf136
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-24 00:26:04 +0300

    update form comment

--- resources/views/back-end/comments/create.blade.php
+++ resources/views/back-end/comments/create.blade.php
@@ -1,5 +1,6 @@
 <form action="{{ route('admin.comment.store') }}" method="POST">
     @csrf
+    <input type="hidden" value="{{ $row->id }}" name="video_id">
     @include('back-end.comments.form')
     <button type="submit" class="btn btn-primary pull-right">إضافة تعليق</button>
     <div class="clearfix"></div>
--- resources/views/back-end/comments/form.blade.php
+++ resources/views/back-end/comments/form.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
-    <input type="hidden" value="{{ $row->id }}" name="video_id">
     @php $input = 'comment' @endphp
     <div class="col-md-12">
         <div class="form-group bmd-form-group">
             <label class="bmd-label-floating">تعليق</label>
-            <textarea class="form-control @error($input) is-invalid @enderror" name="{{ $input }}" id="" cols="30" rows="5" required>{{ old($input, isset($row) ? $row->{$input} : '') }}</textarea>
+            <textarea class="form-control @error($input) is-invalid @enderror" name="{{ $input }}" id="" cols="30"
+                rows="5" required>{{ old($input, isset($row) ? $row->{$input} : '') }}</textarea>
             @include('back-end.shared.error')
         </div>
     </div>
