11c4bbe6a364b9ec5a017f2e9da8820379a27529
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-25 20:52:50 +0300

    add max-height style

--- resources/views/front-end/shared/video-card.blade.php
+++ resources/views/front-end/shared/video-card.blade.php
@@ -1,5 +1,5 @@
 <div class="card" style="width: 20rem;">
-    <img class="card-img-top" src="{{ url('uploads/' . $row->image) }}" alt="Card image cap">
+    <img class="card-img-top" src="{{ url('uploads/' . $row->image) }}" alt="Card image cap" style="max-height: 160px">
     <div class="card-body">
         <p class="card-text">{{ $row->name }}</p>
         <small>{{ $row->created_at }}</small>
