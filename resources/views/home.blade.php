14c679200a880c0f45036d3f1ac84ab0aff29115
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-25 20:34:40 +0300

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        {{ __('You are logged in!') }}
+    <div class="section section-buttons">
+        <div class="container">
+            <div class="title">
+                <h2>آخر الفيديوهات</h2>
+            </div>
+            <div class="row">
+                @foreach ($rows as $row)
+                    <div class="col-lg-4">
+                        @include('front-end.shared.video-card')
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
