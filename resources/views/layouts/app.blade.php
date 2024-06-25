b4be8aa27612914d012947512c422ec38be699ae
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-25 18:21:51 +0300

    create layouts/footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,33 +28,7 @@
 
     <div class="main">
         @yield('content')
-        <footer class="footer footer-black  footer-white ">
-            <div class="container">
-                <div class="row">
-                    <nav class="footer-nav">
-                        <ul>
-                            <li>
-                                <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
-                            </li>
-                            <li>
-                                <a href="http://blog.creative-tim.com/" target="_blank">Blog</a>
-                            </li>
-                            <li>
-                                <a href="https://www.creative-tim.com/license" target="_blank">Licenses</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div class="credits ml-auto">
-                        <span class="copyright">
-                            ©
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script>, made with <i class="fa fa-heart heart"></i> by Mohammed Aljarrah
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        @include('layouts.footer')
 
         <!--   Core JS Files   -->
         <script src="./assets/frontend/js/core/jquery.min.js" type="text/javascript"></script>
