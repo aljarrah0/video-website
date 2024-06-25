8c2f870af10af8d93af5f743fe14828142887627
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-25 20:33:56 +0300

    add Video in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Video;
+
 class HomeController extends Controller
 {
     /**
@@ -21,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $rows = Video::latest()->get(['id', 'name', 'created_at', 'image']);
+
+        return view('home', get_defined_vars());
     }
 }
