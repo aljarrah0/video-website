0ebbfd48c40f09a52121b27006e748df064ebd02
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-25 16:44:36 +0300

    update app page

--- resources/views/back-end/layout/app.blade.php
+++ resources/views/back-end/layout/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- Material Kit CSS -->
-    <link href="/assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+    <link href="/assets/backend/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
     @stack('css')
 </head>
 
@@ -33,23 +33,23 @@
         </div>
     </div>
     <!--   Core JS Files   -->
-    <script src="/assets/js/core/jquery.min.js"></script>
-    <script src="/assets/js/core/popper.min.js"></script>
-    <script src="/assets/js/core/bootstrap-material-design.min.js"></script>
+    <script src="/assets/backend/js/core/jquery.min.js"></script>
+    <script src="/assets/backend/js/core/popper.min.js"></script>
+    <script src="/assets/backend/js/core/bootstrap-material-design.min.js"></script>
     <script src="https://unpkg.com/default-passive-events"></script>
-    <script src="/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <script src="/assets/backend/js/plugins/perfect-scrollbar.jquery.min.js"></script>
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!--  Google Maps Plugin    -->
     {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> --}}
     <!-- Chartist JS -->
-    <script src="/assets/js/plugins/chartist.min.js"></script>
+    <script src="/assets/backend/js/plugins/chartist.min.js"></script>
     <!--  Notifications Plugin    -->
-    <script src="/assets/js/plugins/bootstrap-notify.js"></script>
+    <script src="/assets/backend/js/plugins/bootstrap-notify.js"></script>
     <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="/assets/js/material-dashboard.js?v=2.1.0"></script>
+    <script src="/assets/backend/js/material-dashboard.js?v=2.1.0"></script>
     <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-    <script src="/assets/demo/demo.js"></script>
+    <script src="/assets/backend/demo/demo.js"></script>
     <script>
         $(document).ready(function() {
         $().ready(function() {
