403ab8b74a4c65424ff5bf06922b9c83ad79c56d
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-26 06:48:37 +0300

    add index skills frontend

---
+++ resources/views/front-end/skills/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', $skill->name)
+@section('content')
+    <div class="section section-buttons">
+        <div class="container">
+            <div class="title">
+                <h2>{{ $skill->name }}</h2>
+            </div>
+            <div class="row">
+                @foreach ($videos as $video)
+                    <div class="col-lg-4">
+                        @include('front-end.shared.video-card')
+                    </div>
+                @endforeach
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    {{ $videos->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
