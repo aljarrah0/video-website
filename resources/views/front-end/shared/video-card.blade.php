7ab0537f05289cea0ddeae8fe4c64ed667466ba4
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-06-26 06:30:02 +0300

    update name

--- resources/views/front-end/shared/video-card.blade.php
+++ resources/views/front-end/shared/video-card.blade.php
@@ -1,7 +1,7 @@
 <div class="card" style="width: 20rem;">
-    <img class="card-img-top" src="{{ url('uploads/' . $row->image) }}" alt="Card image cap" style="max-height: 160px">
+    <img class="card-img-top" src="{{ url('uploads/' . $video->image) }}" alt="Card image cap" style="max-height: 160px">
     <div class="card-body">
-        <p class="card-text">{{ $row->name }}</p>
-        <small>{{ $row->created_at }}</small>
+        <p class="card-text">{{ $video->name }}</p>
+        <small>{{ $video->created_at }}</small>
     </div>
 </div>
