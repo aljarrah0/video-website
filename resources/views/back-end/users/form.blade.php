2ca8f3279bdbdb81ed047a303d744369d4c9c8de
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-07-07 20:33:22 +0300

    create new component (name component) & update code

--- resources/views/back-end/users/form.blade.php
+++ resources/views/back-end/users/form.blade.php
@@ -1,12 +1,10 @@
 <div class="row">
-    @php $input = 'name' @endphp
-    <div class="col-md-4">
-        <div class="form-group bmd-form-group">
-            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
-            <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
-            @include('back-end.shared.error')
-        </div>
-    </div>
+    @if(isset($row))
+        <x-name :$row  />
+    @else
+        @php $row = null @endphp
+        <x-name :$row />
+    @endif
     @php $input = 'email' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
---
+++ resources/views/components/name.blade.php
@@ -0,0 +1,9 @@
+@php $input = 'name' @endphp
+<div class="col-md-4">
+    <div class="form-group bmd-form-group">
+        <label class="bmd-label-floating">{{ trans('app.' . $input) }}</label>
+        <input type="text" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}"
+            class="form-control @error($input) is-invalid @enderror">
+        @include('back-end.shared.error')
+    </div>
+</div>
