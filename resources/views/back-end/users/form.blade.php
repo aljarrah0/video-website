4a6f15dbb67c2a83e247b235066d9887ddedf90d
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-07-08 00:19:26 +0300

    create new component (email component)

--- resources/views/back-end/users/form.blade.php
+++ resources/views/back-end/users/form.blade.php
@@ -1,22 +1,16 @@
 <div class="row">
-    @if(isset($row))
-        <x-name :$row  />
+    @if (isset($row))
+        <x-name :$row />
+        <x-email :$row />
     @else
         @php $row = null @endphp
         <x-name :$row />
+        <x-email :$row />
     @endif
-    @php $input = 'email' @endphp
-    <div class="col-md-4">
-        <div class="form-group bmd-form-group">
-            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
-            <input type="email" name="{{ $input }}" value="{{ old($input, isset($row) ? $row->{$input} : '') }}" class="form-control @error($input) is-invalid @enderror">
-            @include('back-end.shared.error')
-        </div>
-    </div>
     @php $input = 'password' @endphp
     <div class="col-md-4">
         <div class="form-group bmd-form-group">
-            <label class="bmd-label-floating">{{ trans('app.'.$input) }}</label>
+            <label class="bmd-label-floating">{{ trans('app.' . $input) }}</label>
             <input type="password" name="{{ $input }}" class="form-control @error($input) is-invalid @enderror">
             @include('back-end.shared.error')
         </div>
